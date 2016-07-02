a956448dd56a17cb8608ff55ff5626b42ce77e09
pakjkwan <pakjkwan@gmail.com>
2016-07-02 18:14:56 +0900

    160702 이미지뷰 완성

--- app/src/main/java/com/hanbit/user/myapp160702/MainActivity.java
+++ app/src/main/java/com/hanbit/user/myapp160702/MainActivity.java
@@ -7,15 +7,17 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity implements View.OnClickListener{
-    Button btCalc, btCalendar;
+    Button btCalc, btCalendar,btImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btCalc = (Button) findViewById(R.id.btCalc);
         btCalendar = (Button) findViewById(R.id.btCalendar);
+        btImageView = (Button) findViewById(R.id.btImageView);
         btCalc.setOnClickListener(this);
         btCalendar.setOnClickListener(this);
+        btImageView.setOnClickListener(this);
     }
 
     @Override
@@ -26,6 +28,9 @@ public class MainActivity extends Activity implements View.OnClickListener{
                break;
            case R.id.btCalendar :
                this.startActivity(new Intent(this,CalendarActivity.class));
+               break;
+           case R.id.btImageView :
+               this.startActivity(new Intent(this,ImageActivity.class));
                break;
        }
     }
